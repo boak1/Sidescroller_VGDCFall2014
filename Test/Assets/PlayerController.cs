80d689b5259638b6de5f3d4c20af9e9fa30be1b0
eaho1 <eaho1@uci.edu>
2014-11-05 17:16:00 -0800

    player test movement
    moo

--- Test/Assets/PlayerController.cs
+++ Test/Assets/PlayerController.cs
@@ -7,15 +7,15 @@ public class PlayerController : MonoBehaviour {
 	void Start () {
 	
 	}
-	int LX = 0f,LY = 0f, nextLX = 10f, nextLY = 0f; //player Coordinates 
+	//int LX = 0f,LY = 0f, nextLX = 10f, nextLY = 0f; //player Coordinates 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.A))
 		{
-			LX = nextLX; LY = nextLY;  //moves player to (Lx, LY)
+			//LX = nextLX; LY = nextLY;  //moves player to (Lx, LY)
 
 			Debug.Log ("a pressed - move player to x location");  //redraw character
-			//gameObject.position(LX,LY, 0f);
+			transform.position = new Vector2(0f, 0f);
 		}
 	}
 }
