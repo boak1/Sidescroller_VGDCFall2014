b2f08cc63aacbd920d655365a660e2cb8410431a
Jason C. <jforeza@gmail.com>
2014-11-05 18:32:14 -0800

    PLATFORM TELEPORTATION!
    three hardcoded platforms that the user can teleport to.  No matter
    where the platforms are moved to, the player will teleport to each
    respective one.
    User inputs: a, s, d

--- Test/Assets/PlayerController.cs
+++ Test/Assets/PlayerController.cs
@@ -2,10 +2,14 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	
+
+	public GameObject platform1;
+	public GameObject platform2;
+	public GameObject platform3;
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	//int LX = 0f,LY = 0f, nextLX = 10f, nextLY = 0f; //player Coordinates 
 	// Update is called once per frame
@@ -14,8 +18,31 @@ public class PlayerController : MonoBehaviour {
 		{
 			//LX = nextLX; LY = nextLY;  //moves player to (Lx, LY)
 
-			Debug.Log ("a pressed - move player to x location");  //redraw character
-			transform.position = new Vector2(0f, 0f);
+			//Debug.Log ("a pressed - move player to x location");  //redraw character
+			transform.position = platform1.transform.position + new Vector3(0f, 5f, 0f);
+		}
+		if (Input.GetKeyDown (KeyCode.S)) 
+		{
+			transform.position = platform2.transform.position + new Vector3(0f, 5f, 0f);
+		}
+		if (Input.GetKeyDown (KeyCode.D)) 
+		{
+			transform.position = platform3.transform.position + new Vector3(0f, 5f, 0f);
 		}
 	}
+
+	/*
+	GameObject Platform(){
+	
+		int offset; //how much higher the player is than the platform
+		int platx = -10f, platy = -10f; //lat and lon for platform
+
+		//new variable for which prefab to use
+
+		//something that describes if and how the platform is moving
+		public Vector2 vec(){
+			return new Vector2(platx, platy);
+		}
+	}
+*/
 }
