b1a4c7e51a5dabf57ec22efc5dafcec6864f03b7
Jason C. <jforeza@gmail.com>
2014-11-18 20:15:00 -0800

    Programming Workshop 11/18

--- Test/Assets/Scripts/LeftWallCollision.cs
+++ Test/Assets/Scripts/LeftWallCollision.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class LeftWallCollision : MonoBehaviour {
 	public Camera mainCam;
+	public GameObject PlatformF;
+	//public GameObject platformHolder;
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
@@ -14,9 +15,9 @@ public class LeftWallCollision : MonoBehaviour {
 	}
 
 	void OnTriggerExit2D (Collider2D hitInfo){
+		/*
 		if (hitInfo.name == "PlatformA") 
 		{
-		
 			hitInfo.transform.position += new Vector3(mainCam.ScreenToWorldPoint(new Vector3(Screen.width*1.65f,0f,0f)).x, 0f, 0f);
 		}
 		if (hitInfo.name == "PlatformS") 
@@ -28,8 +29,31 @@ public class LeftWallCollision : MonoBehaviour {
 			hitInfo.transform.position += new Vector3(mainCam.ScreenToWorldPoint(new Vector3(Screen.width*1.65f,0f,0f)).x, 0f, 0f);
 
 		}
-		if (hitInfo.name == "Player") {
-						Application.LoadLevel ("deathscene"); //set as a level in unity build settings
-				}
+		*/
+
+
+		if (hitInfo.name == "Player") 
+		{
+			Application.LoadLevel ("deathscene"); //set as a level in unity build settings
+		}
+		else if (hitInfo.tag == "Platform"){// && hitInfo.name != "PlatformF(Clone)"){
+			hitInfo.transform.position += new Vector3(mainCam.ScreenToWorldPoint(new Vector3(Screen.width*1.65f,0f,0f)).x, 0f, 0f);
+
+			//GameObject temp = 
+			//GameObject platformHolder = GameObject.Find("PlatformF");
+			//hitInfo.gameObject.SetActive(false);
+			//PlatformF.SetActive(true);
+			//testfind();
+			//platformHolder.transform.position = new Vector3(mainCam.ScreenToWorldPoint(new Vector3(Screen.width*1.65f,10f,0f)).x, 0f, 0f);
+			//Instantiate(PlatformF, new Vector3(mainCam.ScreenToWorldPoint(new Vector3(Screen.width*1.65f,10f,0f)).x, 0f, 0f), Quaternion.identity);
+			//platformHolder = hitInfo.gameObject;
+		}
+
 	}
+
+
+	public void testfind(){
+		//platformHolder = GameObject.Find ();
+	}
+
 }
