9282f800ff78e432a9a508669057eb5e82e19456
Vincent Chang <vincentchang9999@gmail.com>
2014-11-18 16:56:44 -0800

    Looping Music test part 2
    Changes:
    --added better-cut clips to the bgm & sfx assets, deleted the old ones
    --disabled 3D Sound on the clips so now bgm is non-diegetic and plays
    back in stereo (also set BGMPlayer's Z-position to 0 just in case)
    --tweaked the script and got rid of the 2-second delay between game
    startup and music start
    --added comments to the script so hopefully others can figure it out
    --got rid of "Test Scene 1 clone.scene" because it's an old version of
    marrowGroveMusicTestscene
    --renamed the scene to marrowGrove instead of morrowGrove, whoops

--- Test/Assets/Scripts/bgmMergeClipsAndLoop.cs
+++ Test/Assets/Scripts/bgmMergeClipsAndLoop.cs
@@ -1,22 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-/*I literally copied this from the Unity doc website so I don't fully understand how it works 
+/* I literally copied this from the Unity doc website, so hopefully it'll work well enough for now
  *       --Vincent
  * 
- * Plays clip1, starts loading clip2 into buffer 1 second before clip2 starts playing
- * After that, keeps loading clip2 into buffer 1 second before clip2 ends
+ * Uses an array of 2 audio clips in Audioclip[] clip
+ * Loads clip[0] into memory and starts playback upon game startup
+ * One second before clip[0] reaches the end, loads clip[1] into memory buffer
+ * After clip[1] starts playing, it will keep loading and looping clip[1]
  * 
- * TODO:
- * make clips playback in stereo, not mono
- * make clips playback at correct volume; right now they play back at a very low volume
- * cut better loop clips from original track .wav export
+ * NOTE: script is dependent on variables bpm and numBeatsPerSegment for timing the loading and playback of clips
  */
 
 [RequireComponent(typeof(AudioSource))]
 public class bgmMergeClipsAndLoop : MonoBehaviour {
-	public float bpm = 120.0F;
-	public int numBeatsPerSegment = 224;
+	public float bpm = 120.0F;				//both clips are at 120 bpm
+	public int numBeatsPerSegment = 224;	//each clip is 56 measures long; 56 measures * 4 beats per measure = 224
 	public AudioClip[] clips = new AudioClip[2];
 	private double nextEventTime;
 	private int index = 0;
@@ -31,7 +30,7 @@ public class bgmMergeClipsAndLoop : MonoBehaviour {
 			audioSources[i] = child.AddComponent("AudioSource") as AudioSource;
 			i++;
 		}
-		nextEventTime = AudioSettings.dspTime + 2.0F;
+		nextEventTime = AudioSettings.dspTime;	//add a float to this line to specify a delay b/w game startup and music playback
 		running = true;
 	}
 
@@ -45,7 +44,7 @@ public class bgmMergeClipsAndLoop : MonoBehaviour {
 			audioSources[index].PlayScheduled(nextEventTime);
 			Debug.Log("Scheduled source " + index + " to start at time " + nextEventTime);
 			nextEventTime += 60.0F / bpm * numBeatsPerSegment;
-			index = 1;
+			index = 1;		//after clip[0] plays, we only need to play clip[1] until game exit
 		}
 	}
 }
