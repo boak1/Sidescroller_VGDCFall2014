9e30178c58c28a66600361d1bb9aa096fa2289c9
boak@uci.edu <boak@uci.edu>
2014-11-21 17:58:10 -0800

    laser mostly done

--- Test/Assets/healthScript.cs
+++ Test/Assets/healthScript.cs
@@ -8,13 +8,13 @@ public class healthScript : MonoBehaviour {
 	public Sprite oneHitHP;
 	public Sprite twoHitHP; 
 	//used to change sprites
-	private SpriteRenderer spriteRenderer; 
+	private SpriteRenderer spriteRendererH; 
 	// Use this for initialization
 	void Start () {
 
-		spriteRenderer = GetComponent<SpriteRenderer>(); //  the SpriteRenderer that is attached to the Gameobject
-		if (spriteRenderer.sprite == null) // if the sprite  is empty// set the sprite to fullHP
-			spriteRenderer.sprite = fullHp; 
+		spriteRendererH = GetComponent<SpriteRenderer>(); //  the SpriteRenderer that is attached to the Gameobject
+		if (spriteRendererH.sprite == null) // if the sprite  is empty// set the sprite to fullHP
+			spriteRendererH.sprite = fullHp; 
 	
 	}
 	
@@ -23,12 +23,12 @@ public class healthScript : MonoBehaviour {
 		int hpTest = PlayerController.hp;
 
 		if ( hpTest== 2) {
-			spriteRenderer.sprite= oneHitHP;//change the sprite to 2 hp
+			spriteRendererH.sprite= oneHitHP;//change the sprite to 2 hp
 				}
-		if (hpTest == 1) {
-			spriteRenderer.sprite= twoHitHP; // chnage the sprite to 1 hp
+		else if (hpTest == 1) {
+			spriteRendererH.sprite= twoHitHP; // chnage the sprite to 1 hp
 		}
-		if (hpTest == 0) {
+		else if (hpTest == 0) {
 			Application.LoadLevel ("deathscene"); //moves to game over screen
 			
 		}
---
+++ Test/Assets/laserScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class laserScript : MonoBehaviour {
+	//these are picutres of sprites
+	public Sprite preLaser;
+	public Sprite postLaser;
+	private float cooldown;
+	public float cooldownStart;
+
+	private SpriteRenderer spriteRenderL;
+
+	// Use this for initialization
+	void Start () {
+		//spriteRenderL = GetComponent<SpriteRenderer>();
+		//if (spriteRenderL.sprite == null)
+		//spriteRenderL.sprite = preLaser;
+		cooldown = cooldownStart;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cooldown -= Time.deltaTime;
+		if (cooldown == 1) {
+			spriteRenderL.sprite = preLaser;
+
+				} 
+		else if (cooldown == 0) {
+			spriteRenderL.sprite = postLaser;
+			cooldown = cooldownStart;
+				} 
+		else {
+			spriteRenderL.sprite = null;
+				}
+
+	
+	}
+
+}
