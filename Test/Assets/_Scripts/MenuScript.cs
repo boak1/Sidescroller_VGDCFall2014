fd721e925d2c4a5bcdedd593f077e00abbcadd38
Alex561 <alexchapp@yahoo.com>
2014-12-05 19:14:47 -0800

    Menu screen with placeholder sprite

---
+++ Test/Assets/_Scripts/MenuScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			Application.LoadLevel ("testScene");
+		}
+		
+	}
+}
